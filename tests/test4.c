72ef3143a234c0b855eee6e94fe1be47afd59e80
M1-Elmasry <mostafamfelmasry@outlook.com>
2023-03-28 03:05:54 +0200

    add test file for case_s function

---
+++ tests/test4.c
@@ -0,0 +1,13 @@
+#include "../main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+    int r = _printf("%S\n", "Best\tSchool");
+    _printf("%d", r);
+    return (0);
+}
